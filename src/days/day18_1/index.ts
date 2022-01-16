b75d628f2d61664da4c0e57860d80d3111d7bab3
Sébastien Chédor <sebastien.chedor@checkandvisit.com>
2022-01-16 23:47:01 +0100

    day 18

---
+++ src/days/day18_1/index.ts
@@ -0,0 +1,6 @@
+export { parse_entry } from "./parse_entry";
+export { compute } from "./compute";
+
+export { example_input } from "./tests/example_input";
+export { example_output } from "./tests/example_output";
+export { real_input } from "./tests/real_input";
